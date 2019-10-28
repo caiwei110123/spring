6f88cdbcbf81ec0c3a3b19e8e103d64166c992ce
yilie <yilie.cw@alibaba-inc.com>
2019-10-28 10:27:36 +0800

    订单

---
+++ spring-design-pattern/src/main/java/pattern/observer/aa.java
@@ -0,0 +1,13 @@
+package pattern.observer;
+
+/**
+ * 功能描述：
+ *
+ * @date : 2019/10/23  下午4:21
+ * @Version 1.0
+ */
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("发么开发");
+    }
+}
